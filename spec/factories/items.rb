0fa296cf434bb5620c5327b0cf1a5e7fe7ae6200
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-14 12:37:14 +0000

    Implement faker to factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1.5 }
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { 10000.0 }
   end
 end
