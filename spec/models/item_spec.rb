93ff2adf93f55beb118869cc1038a4ae7ae2cd4f
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-14 12:47:18 +0000

    Add spec for Item model, is valid with a name and a price

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Item, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:item)).to be_valid
   end
+
+  it 'is valid with a name and a price' do
+    expect(FactoryBot.build(:item)).to be_valid
+  end
 end
