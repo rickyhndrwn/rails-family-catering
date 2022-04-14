02881699aee1687457eb0fb0832c348bc78fc8f4
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-14 12:49:20 +0000

    Add spec for Item model, is invalid without a name

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Item, type: :model do
   it 'is valid with a name and a price' do
     expect(FactoryBot.build(:item)).to be_valid
   end
+
+  it 'is invalid without a name' do
+    item = FactoryBot.build(:item, name: nil)
+
+    item.valid?
+
+    expect(item.errors[:name]).to include("can't be blank")
+  end
 end
