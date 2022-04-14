ebbd102be76a7d0baf5ddd8da31e79add03bf710
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-14 12:57:21 +0000

    Add spec for Item model, is invalid with a duplicate name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :price, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,4 +20,11 @@ RSpec.describe Item, type: :model do
     item.valid?
     expect(item.errors[:price]).to include("can't be blank")
   end
+
+  it "is invalid with a duplicate name" do
+    item1 = FactoryBot.create(:item, name: 'Nasi Uduk')
+    item2 = FactoryBot.build(:item, name: 'Nasi Uduk')
+    item2.valid?
+    expect(item2.errors[:name]).to include("has already been taken")
+  end
 end
