ac796bd4fb7e8c330ab3e08d55fea5e36e32c2b2
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-14 12:55:07 +0000

    Add spec for Item model, is invalid without a price

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   validates :name, presence: true
+  validates :price, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe Item, type: :model do
 
   it 'is invalid without a name' do
     item = FactoryBot.build(:item, name: nil)
-
     item.valid?
-
     expect(item.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a price' do
+    item = FactoryBot.build(:item, price: nil)
+    item.valid?
+    expect(item.errors[:price]).to include("can't be blank")
   end
 end
