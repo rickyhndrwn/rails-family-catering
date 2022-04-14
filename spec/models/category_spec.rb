60d890c3cfcf3b2e4a5bf0d3eab49be88d794289
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-14 13:46:02 +0000

    Add spec for Category model, is valid with a name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Category, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:category)).to be_valid
   end
+
+  it 'is valid with a name' do
+    expect(FactoryBot.build(:category)).to be_valid
+  end
 end
