fdfea52d3092315cfee86c42b968088c6a41e471
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:19:29 +0000

    Implement faker to factory of ItemCategory

--- spec/factories/item_categories.rb
+++ spec/factories/item_categories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item_category do
-    item_id { 1 }
-    category_id { 1 }
+    item_id { Faker::Number.within(range: 1..10) }
+    category_id { Faker::Number.within(range: 1..10) }
   end
 end
