53f682bae9d882094bf3a7bce65a54686ffc3343
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:50:05 +0000

    Add spec for index controller of Item

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController do
+  describe 'GET #index' do
+    it "populates an array of all items" do 
+      nasi_uduk = create(:item, name: "Nasi Uduk")
+      kerak_telor = create(:item, name: "Kelar Telor")
+      get :index
+      expect(assigns(:items)).to match_array([nasi_uduk, kerak_telor])
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
