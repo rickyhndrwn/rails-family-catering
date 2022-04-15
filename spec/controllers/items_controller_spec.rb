d92196eab3311f077118119e02c4ed805fd4fc1d
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:55:12 +0000

    Add spec for edit controller of Item

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -39,4 +39,18 @@ RSpec.describe ItemsController do
       expect(:response).to render_template :new
     end
   end
+
+  describe 'GET #edit' do
+    it "assigns the requested item to @item" do
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(assigns(:item)).to eq item
+    end
+
+    it "renders the :edit template" do
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(response).to render_template :edit
+    end
+  end
 end
