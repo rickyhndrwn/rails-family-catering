0f4c427546d607330a12da5a84ed60cd0d7b5ce1
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 15:24:17 +0000

    Update item controller, adding skip_before_action :verify_authenticity_token

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_item, only: %i[ show edit update destroy ]
 
   # GET /items or /items.json
