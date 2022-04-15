2d189d2b9758e77c9ebb69569cd10d52e5192171
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:21:47 +0000

    Add spec for ItemCategory model, is valid with an item id and a category id

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ItemCategory, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:item)).to be_valid
   end
+
+  it 'is valid with an item id and a category id' do
+    expect(FactoryBot.build(:item)).to be_valid
+  end
 end
