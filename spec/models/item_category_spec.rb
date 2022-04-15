2bbdc990eec81c5fd5f7ef7d4036a0737abdf28b
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:37:18 +0000

    Remove spec for ItemCategory model, both has a valid factory and is valid with an item id and a category id

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
-  it 'has a valid factory' do
-    expect(FactoryBot.build(:item_category)).to be_valid
-  end
+  # it 'has a valid factory' do
+  #   expect(FactoryBot.build(:item_category)).to be_valid
+  # end
 
-  it 'is valid with an item id and a category id' do
-    expect(FactoryBot.build(:item_category)).to be_valid
-  end
+  # it 'is valid with an item id and a category id' do
+  #   expect(FactoryBot.build(:item_category)).to be_valid
+  # end
 
   it 'is invalid without belongs_to association to Item' do
     item_category = ItemCategory.reflect_on_association(:items).macro
