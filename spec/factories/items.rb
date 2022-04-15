57876ef398272cddde6294f1941778a4822ad4b0
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:58:42 +0000

    Add invalid factory for Item

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     description { Faker::Food.description[0..50] }
     price { 10000.0 }
   end
+
+  factory :invalid_item, parent: :item do
+    name { nil }
+    description { nil }
+    price { 10000.0 }
+  end
 end
