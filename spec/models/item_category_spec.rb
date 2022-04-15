5c755cfaa158b2a37c3b7bfa7159e3aec0ee2d32
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:25:04 +0000

    Correcting spec for ItemCategory, wrong factory was called

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
   it 'has a valid factory' do
-    expect(FactoryBot.build(:item)).to be_valid
+    expect(FactoryBot.build(:item_category)).to be_valid
   end
 
   it 'is valid with an item id and a category id' do
-    expect(FactoryBot.build(:item)).to be_valid
+    expect(FactoryBot.build(:item_category)).to be_valid
   end
 end
