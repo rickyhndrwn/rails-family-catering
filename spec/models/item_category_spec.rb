56c293ccb5873ddaa7191409dc5f40375b97bfa9
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-15 01:20:31 +0000

    Add spec for ItemCategory model, has a valid factory

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:item)).to be_valid
+  end
 end
