684a3f922fee4c9605a25fdadfb0b82229898272
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-16 16:24:37 +0000

    Add spec for index controller of Customer

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController do
+  describe 'GET #index' do
+    it "populates an array of all customers" do 
+      nasi_uduk = create(:customer, name: "Nasi Uduk")
+      kerak_telor = create(:customer, name: "Kelar Telor")
+      get :index
+      expect(assigns(:customers)).to match_array([nasi_uduk, kerak_telor])
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
