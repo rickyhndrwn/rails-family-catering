fd5ee0c682e588b915b75bb942257ec043ad983e
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-16 15:58:39 +0000

    Add spec for Customer, is valid with a name and an email

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Customer, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:customer)).to be_valid
   end
+
+  it 'is valid with a name and an email' do
+    expect(FactoryBot.build(:customer)).to be_valid
+  end
 end
