793869fcb2dcaba4025c7e7a6ab8d0a1577ee363
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2022-04-16 14:45:54 +0700

    Add spec for index controller of Category

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController do
+  describe 'GET #index' do
+    it "populates an array of all categories" do 
+      food = create(:category)
+      drink = create(:category, name: "Drink")
+      get :index
+      expect(assigns(:categories)).to match_array([food, drink])
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
