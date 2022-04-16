9942db0351796fb9faf08116ec2f0f8bed684f94
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2022-04-16 20:16:44 +0700

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
-  has_many :categorizations
+  has_many :categorizations, dependent: :destroy
   has_many :items, through: :categorizations
 end
