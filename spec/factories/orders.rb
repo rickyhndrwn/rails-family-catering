f8ff025d03aa290d27e76fb72a6ae9345754410d
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-17 10:18:56 +0000

    Implement faker on Order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :order do
-    order_date { "MyString" }
+    order_date { Faker::Date.between(from: '2014-09-23', to: '2015-09-25') }
     total_price { 1.5 }
     status { "MyString" }
   end
