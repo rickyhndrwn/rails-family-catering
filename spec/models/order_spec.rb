9a52bfbabe9d8bec7f9fce9dcef3f28adbe2ca25
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-17 10:24:12 +0000

    Add spec for Order, has a valid factory and valid params

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:order)).to be_valid
+  end
+
+  it 'is valid with an order date, a total price, and a status' do
+    expect(FactoryBot.build(:order)).to be_valid
+  end
 end
