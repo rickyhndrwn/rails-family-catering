deffdbaeadf65862cb533c2c44aa17104dd67edd
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-19 04:03:46 +0000

    Cart model set value before save

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  before_save :set_price, :set_subtotal
+
+	def set_price
+		self.price = self.menu.price
+	end
+
+	def set_subtotal
+		self.sub_total = self.quantity * self.price
+	end
 end
