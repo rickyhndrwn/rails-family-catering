a482ed312cf9adfbf0cf10c05ffbbb860ffd0434
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2022-04-21 19:37:20 +0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Category.create(name: 'Breakfast')
-Category.create(name: 'Lunch')
-Category.create(name: 'Dinner')
-Category.create(name: 'Beverages')
+cat1 = Category.create(name: 'Breakfast')
+cat2 = Category.create(name: 'Lunch')
+cat3 = Category.create(name: 'Dinner')
+cat4 = Category.create(name: 'Beverages')
+
+Item.create(
+  name: 'Rujak',
+  description: 'Salad dish of Javanese origin, commonly found in Indonesia, Malaysia and Singapore.',
+  price: 50,
+  category_ids: [cat1, cat2]
+)
+
+Item.create(
+  name: 'Tahu',
+  description: 'Prepared by coagulating soy milk and then pressing the resulting curds into solid white blocks of varying softness.',
+  price: 30,
+  category_ids: [cat1, cat2, cat3]
+)
+
+Item.create(
+  name: 'Soda Gembira',
+  description: 'Made up of carbonated water, condensed milk, syrup and ice.',
+  price: 25,
+  category_ids: [cat4]
+)
+
+Item.create(
+  name: 'Teh',
+  description: 'Aromatic beverage prepared by pouring hot or boiling water over cured or fresh leaves of Camellia sinensis.',
+  price: 15,
+  category_ids: [cat4]
+)
 
 Customer.create(name: 'Ali', email: 'ali@email.com')
 Customer.create(name: 'Boa', email: 'boa@email.com')
+Customer.create(name: 'Coy', email: 'coy@email.com')
+Customer.create(name: 'Dea', email: 'dea@email.com')
