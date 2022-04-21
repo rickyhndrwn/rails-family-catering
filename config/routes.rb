e545d9391b5e4ff289c877a7bd76c35e90c60d8a
Ricky Hendrawan <ricky.hndrwn98@gmail.com>
2022-04-21 11:41:48 +0700

    Change default/root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "items#index"
+  root "orders#index"
   
   post 'orders/update_order_status'
 end
